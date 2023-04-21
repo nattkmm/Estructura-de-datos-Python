a9f7925ec127c2a712e99624b85a8c34187bcc2c
natt <nattkmm@users.noreply.github.com>
2023-04-21 11:27:35 -0400

    reto 21 de abril

---
+++ reto_3_2.py
@@ -0,0 +1,14 @@
+palabra = input ("ingrese una palabra: \n")
+
+cantidad_vocales ={ 'a':0 , 'e':0 , 'i':0 , 'o':0 , 'u':0 }
+
+for i in palabra:
+    if i.lower() in cantidad_vocales:
+        cantidad_vocales [i.lower()] +=1
+
+print ("La cantidad de vocales en", palabra, "son:")
+print ("a = ", cantidad_vocales ['a'])
+print ("e = ", cantidad_vocales ['e'])
+print ("i = ", cantidad_vocales ['i'])
+print ("o = ", cantidad_vocales ['o'])
+print ("u = ", cantidad_vocales ['u'])
